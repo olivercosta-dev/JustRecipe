536a29723ac7e53c1d7ed404ee4a3dccb48b1359
Oliver <costaolika@gmail.com>
2024-02-25 18:51:32 +0100

    first nav appear animation

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const navBarElement = document.querySelector('.navigation')
+const landingPageHeader = document.querySelector('header')
+
+console.log(navBarElement)
+
+const navObserver = 
+    new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting){
+                navBarElement.classList.remove('hidden')
+            } else {
+                navBarElement.classList.add('hidden')
+            }
+        }),
+        {
+            threshold: 0.0
+        }
+        }
+    )
+
+
+navObserver.observe(landingPageHeader)
