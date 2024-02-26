38eac6061c136ed9ce7f97d13ff44a371ad807c6
Oliver <costaolika@gmail.com>
2024-02-26 19:43:23 +0100

    express-embrace-grow animations w/ sidebar done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,13 @@
 const navBarElement = document.querySelector('.dot-menu')
 const landingPageHeader = document.querySelector('header')
 
-console.log(navBarElement)
+const sushiElement = document.querySelector('.sushi-wrapper')
+const exploreButtonElement = document.querySelector('.explore-wrapper button')
+const chopstickElement = document.querySelector('.chopstick-wrapper')
+const createButtonElement = document.querySelector('.create-wrapper button')
+
+const expressEmbraceGrowElement = document.querySelector('.express-embrace-grow')
+const progressBarElement = document.querySelector('.progress-bar')
 
 const navObserver = 
     new IntersectionObserver((entries, observer) => {
@@ -15,8 +21,81 @@ const navObserver =
         {
             threshold: 0.0
         }
-        }
-    )
+    }
+)
 
+// const progressBarObserver = 
+//         new IntersectionObserver((entries, observer) => {
+//             console.log(entries)
+//             entries.forEach(entry => {
+//                 if(entry.isIntersecting){
+//                     document.addEventListener('scroll',(event) => {
+//                         onProgresScroll(event)
+//                     })
+//                 }
+//             })
+//         }
+// )
+document.addEventListener('scroll',(event) => {
+    onProgressScroll(event)
+})
+
+function onProgressScroll(event) {
+    const documentRelativePositionY = expressEmbraceGrowElement.getBoundingClientRect().top + document.documentElement.scrollTop
+    const scrollYWithOffset = window.scrollY + 400
+    if (
+         scrollYWithOffset >= documentRelativePositionY &&
+         scrollYWithOffset <= 
+         documentRelativePositionY + expressEmbraceGrowElement.getBoundingClientRect().height
+        ) {
+            console.log('We are in!')
+            const distanceFromTopOfElement = scrollYWithOffset - documentRelativePositionY
+            const percentageScrolled = (distanceFromTopOfElement / expressEmbraceGrowElement.getBoundingClientRect().height) * 100 
+            progressBarElement.style.height = (percentageScrolled) + '%'
+            
+            const currentRow = Math.floor(percentageScrolled / 33)
+            
+            console.log(currentRow)
+            const fadedElements = 
+                document
+                .querySelectorAll
+                    (`.express-embrace-grow>:not(.progress-bar,:nth-child(${1 + currentRow * 2 + 1}),:nth-child(${1 + currentRow * 2 + 2}))`);
+            let fadedArray = Array.from(fadedElements)            
+            let childrenOfExpressEmbraceGrow = document.querySelectorAll('.express-embrace-grow>*:not(.progress-bar)')
+            childrenOfExpressEmbraceGrow.forEach(e => {
+                if(fadedArray.includes(e)) {
+                    if (!e.classList.contains('faded')){
+                        e.classList.add('faded')
+                    }
+                } else {
+                    if (e.classList.contains('faded')){
+                        e.classList.remove('faded')
+                    }
+                }
+            })
+            if (currentRow === 0){
+                progressBarElement.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--red-color').trim();
+            }
+            else if (currentRow === 1){
+                progressBarElement.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--green-color').trim();
+            } else {
+                progressBarElement.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--beige-color').trim();
+            }
+        }
+}
 
 navObserver.observe(landingPageHeader)
+// progressBarObserver.observe(expressEmbraceGrowElement)
+
+exploreButtonElement.addEventListener('click',event => {
+    sushiElement.classList.toggle('swapped')
+    exploreButtonElement.classList.toggle('swapped')
+})
+
+createButtonElement.addEventListener('click',event => {
+    const ingredientImage = chopstickElement.querySelector('.ingredients')
+    const preparedFoodImage = chopstickElement.querySelector('.prepared-food')
+    ingredientImage.classList.toggle('hidden')
+    preparedFoodImage.classList.toggle('hidden')
+})
+
